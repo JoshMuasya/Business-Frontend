fba9ff05190c896674a24f6ef2bcecab7bfe84eb
JoshMuasya <muasyajoshua07@gmail.com>
2023-08-02 12:06:59 +0300

    Intergrated feenote view page with backend

--- src/app/home/feenotes/view/page.jsx
+++ src/app/home/feenotes/view/page.jsx
@@ -1,10 +1,28 @@
 "use client"
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Link from 'next/link';
 
 const page = () => {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/feenote/');
+        const jsonData = await response.json();
+        setData(jsonData);
+        console.log(jsonData)
+      } catch {
+        console.error('Error fetching data', data);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     // Main
     <div className='w-full mt-28 p-5 flex flex-col justify-center align-middle items-center text-center'>
@@ -14,255 +32,54 @@ const page = () => {
       </div>
 
       {/* Body */}
-      <div className='flex flex-col justify-between w-full'>
-        {/* Feenote row 1 */}
-        <div className='flex flex-col sm:flex-row justify-center items-center align-middle sm:pb-3 md:pb-8'>
-          {/* Left */}
-          <div className='flex flex-col mb-5 sm:mb-0 sm:mr-5 md:mr-8 p-3 h-fit w-fit bg-thistle rounded-xl '>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
+      <div className='flex flex-col sm:flex-row sm:flex-wrap justify-between w-full'>
+        {/* Display Feenotes */}
+        {data.map((feenote, index) => (
+          <div 
+            className='flex flex-col sm:flex-row justify-center items-center align-middle sm:pb-3 md:pb-8'
+            key={index}
+          >
+            {/* Feenotes */}
+            <div className='flex flex-col mb-5 sm:mb-0 sm:mr-5 md:mr-8 p-3 h-fit w-fit bg-thistle rounded-xl '>
+              {/* Company Name */}
+              <h1 className='text-backblack text-m font-kalam font-bold'>
+                {feenote.company_name}
+              </h1>
 
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
+              {/* Customer Name */}
+              <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
+                {feenote.customer_name}
+              </h3>
 
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href="/home/feenotes/view/1"
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
+              {/* Buttons */}
+              <div className='flex flex-row justify-center items-center align-middle'>
+                {/* View */}
+                <Link
+                  href={`/home/feenotes/view/${feenote.id}`}
+                  className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
+                >
+                  View
+                </Link>
 
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
+                {/* Update */}
+                <Link 
+                  href=""
+                  className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
+                >
+                  Update
+                </Link>
 
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
+                {/* Delete */}
+                <Link
+                  href=""
+                  className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
+                >
+                  Delete
+                </Link>
+              </div>
             </div>
           </div>
-
-          {/* Right */}
-          <div className='flex flex-col mb-5 sm:mb-0 p-3 h-fit w-fit bg-thistle rounded-xl'>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
-
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
-
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
-
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
-
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        {/* Feenote row 2 */}
-        <div className='flex flex-col sm:flex-row justify-center items-center align-middle sm:pb-3 md:pb-8'>
-          {/* Left */}
-          <div className='flex flex-col mb-5 sm:mb-0 sm:mr-5 md:mr-8 p-3 h-fit w-fit bg-thistle rounded-xl '>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
-
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
-
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
-
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
-
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
-            </div>
-          </div>
-
-          {/* Right */}
-          <div className='flex flex-col mb-5 sm:mb-0 p-3 h-fit w-fit bg-thistle rounded-xl'>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
-
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
-
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
-
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
-
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        {/* Feenote row 3 */}
-        <div className='flex flex-col sm:flex-row justify-center items-center align-middle sm:pb-3 md:pb-8'>
-          {/* Left */}
-          <div className='flex flex-col mb-5 sm:mb-0 sm:mr-5 md:mr-8 p-3 h-fit w-fit bg-thistle rounded-xl '>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
-
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
-
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
-
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
-
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
-            </div>
-          </div>
-
-          {/* Right */}
-          <div className='flex flex-col mb-5 sm:mb-0 p-3 h-fit w-fit bg-thistle rounded-xl'>
-            {/* Title */}
-            <h1 className='text-backblack text-m font-kalam font-bold'>
-              Feenote 1
-            </h1>
-
-            {/* Subtitle */}
-            <h3 className='text-backblack font-kalam font-semibold text-s pb-2'>
-              Customer Name
-            </h3>
-
-            {/* Buttons */}
-            <div className='flex flex-row justify-center items-center align-middle'>
-              {/* View */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                View
-              </Link>
-
-              {/* Update */}
-              <Link 
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Update
-              </Link>
-
-              {/* Delete */}
-              <Link
-                href=""
-                className='bg-buttontext text-buttonback hover:bg-buttonback hover:text-buttontext font-kalam text-xs px-5 py-2 mr-2 rounded-2xl'
-              >
-                Delete
-              </Link>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   )
