3b3e00f9fece5a480d621e618a3eda7cb78504bb
JoshMuasya <muasyajoshua07@gmail.com>
2023-08-02 11:35:56 +0300

    Intergrated Add Function with Backend

--- src/app/home/feenotes/add/page.jsx
+++ src/app/home/feenotes/add/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React from 'react';
+import React, { forwardRef, useState } from 'react';
 
 import BusinessIcon from '@mui/icons-material/Business';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
@@ -12,7 +12,59 @@ import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
 import CardMembershipIcon from '@mui/icons-material/CardMembership';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
 const page = () => {
+
+  const [company_name, setcompany_name] = useState('');
+  const [company_address, setcompany_address] = useState('');
+  const [feenote_number, setfeenote_number] = useState('');
+  const [customer_name, setcustomer_name] = useState('');
+  const [reference, setreference] = useState('');
+  const [total_amount, settotal_amount] = useState('');
+  const [amount_paid, setamount_paid] = useState('');
+  const [payment_details, setpayment_details] = useState('');
+  const [sign_off, setsign_off] = useState('');
+
+  const [open, setOpen] = useState(false);
+  const [responseStatus, setResponseStatus] = useState(null);
+
+  const Alert = forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant='filled' {...props} />;
+  });
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    setOpen(true);
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/feenote/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ company_name, company_address, feenote_number, customer_name, reference, total_amount, amount_paid, payment_details, sign_off })
+      });
+
+      if (response.ok) {
+        console.log('Feenote Added')
+        console.log(response.ok)
+        setResponseStatus(response.ok)
+      } else {
+        console.log('Failed')
+      }
+    } catch (error) {
+      console.error('Adding Error:', error);
+    }
+  }
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  }
+
   return (
     <div className='flex flex-col justify-center align-middle items-center'>
       {/* Title */}
@@ -22,9 +74,35 @@ const page = () => {
 
       {/* Body */}
       <div className='bg-white h-fit w-4/5 rounded-xl sm:rounded-3xl pb-2 mb-12'>
+
+        {/* SnackBar */}
+        <Snackbar
+          open = {open}
+          autoHideDuration={3000}
+          onClose={handleClose}
+        >
+          {
+            responseStatus === true ? (
+              <Alert 
+                onClose={handleClose} 
+                severity="success"
+                sx={{ width: "100%" }}
+              >
+                The Feenote was added Successfully
+              </Alert>
+            ) : (
+              <Alert 
+                severity="error"
+              >
+                Failed!! The Feenote was not Added Successfully
+              </Alert>
+            )
+          }          
+        </Snackbar>
+
         {/* Form */}
         <form 
-          onSubmit="" 
+          onSubmit={handleAdd} 
           className="p-5 sm:p-7 flex flex-col justify-center align-middle items-center"
         >
           <div className="relative mb-4 sm:mb-6 w-4/5">
@@ -42,6 +120,8 @@ const page = () => {
               id="companyname" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-8 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
               placeholder="Company Name"
+              value = {company_name}
+              onChange={(e) => setcompany_name(e.target.value)}
             />
           </div>
 
@@ -58,7 +138,9 @@ const page = () => {
             <textarea 
               id="companyaddress" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-10 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
-              placeholder="Company Address"              
+              placeholder="Company Address"
+              value={company_address}           
+              onChange={(e) => setcompany_address(e.target.value)}
             />
           </div>
 
@@ -77,7 +159,8 @@ const page = () => {
               id="feenotenumber" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-8 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
               placeholder="Feenote Number" 
-              
+              value={feenote_number}
+              onChange={(e) => setfeenote_number(e.target.value)}
             />
           </div>
 
@@ -96,7 +179,8 @@ const page = () => {
               id="customername" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-8 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
               placeholder="Customer Name" 
-              
+              value={customer_name}
+              onChange={(e) => setcustomer_name(e.target.value)}              
             />
           </div>
 
@@ -114,7 +198,8 @@ const page = () => {
               id="details" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-10 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
               placeholder="Feenote Details" 
-              
+              value={reference}
+              onChange={(e) => setreference(e.target.value)}              
             />
           </div>
 
@@ -135,7 +220,8 @@ const page = () => {
                 id="totalamount" 
                 className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-8 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
                 placeholder="Total Amount" 
-                
+                value={total_amount}
+                onChange={(e) => settotal_amount(e.target.value)}                
               />
             </div>
 
@@ -153,8 +239,9 @@ const page = () => {
                 type="number" 
                 id="amountpaid" 
                 className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-8 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
-                placeholder="Amount Paid" 
-                
+                placeholder="Amount Paid"
+                value={amount_paid}
+                onChange={(e) => setamount_paid(e.target.value)}                
               />
             </div>
           </div>
@@ -172,8 +259,9 @@ const page = () => {
             <textarea 
               id="paymentdetails" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-10 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
-              placeholder="Payment Details" 
-              
+              placeholder="Payment Details"
+              value={payment_details}
+              onChange={(e) => setpayment_details(e.target.value)}              
             />
           </div>
 
@@ -190,8 +278,9 @@ const page = () => {
             <textarea 
               id="signoff" 
               className="dark:text-gray-900 text-xs sm:text-sm rounded-lg focus:ring-blue-500 block w-full pl-10 sm:pl-10 p-2 sm:p-2.5 border-darkgray placeholder-gray text-gray" 
-              placeholder="Sign Off" 
-              
+              placeholder="Sign Off"
+              value={sign_off}
+              onChange={(e) => setsign_off(e.target.value)}              
             />
           </div>
 
